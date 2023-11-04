394ebf7462afadcdbdc05c74dece9cf46d2e5c0a
vmarc <vmarcial8209@gmail.com>
2023-11-04 13:28:49 -0700

    changes for animations

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
-{
+{   public  Animator animator;
     private float horizontal;
     public float speed = 8f;
     public float jumpPower = 15f; 
     public float acceleration;
     public float targetSpeed = 20f;
     private bool IsFacingRight;
+    public bool isJumping;
+ 
+   
+    
+    
+    
+   
 
     [SerializeField] private Rigidbody2D rigidBody;
     [SerializeField] private Transform groundCheck;
@@ -20,35 +27,50 @@ public class PlayerControl : MonoBehaviour
     void Start()
     {
         IsFacingRight = true;
+       
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+      
+    animator.SetBool("isJumping",false);
+    isJumping = false;
+
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpPower);
+            animator.SetBool("isJumping",true);
+            isJumping = true;
         }
+        
 
         if (Input.GetButtonUp("Jump") && rigidBody.velocity.y > 0f)
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y * 0.5f);
+             animator.SetBool("isJumping",true);
+             isJumping = true;
         }
+          
+
 
         Turn();
+        
     }
 
     void FixedUpdate()
-    {
+    {   
         horizontal = Input.GetAxisRaw("Horizontal");
+        
         rigidBody.velocity = new Vector2(horizontal * speed, rigidBody.velocity.y);
-
+        
     }
 
     //what about other platforms that aren't necessarily the ground?
     private bool IsGrounded()
-    {
+    { 
         return Physics2D.OverlapBox(groundCheck.position, groundCheckSize, groundLayer);
+       
     }
 
     private void Turn()
@@ -61,6 +83,8 @@ public class PlayerControl : MonoBehaviour
 
             IsFacingRight = !IsFacingRight;
         }
+           
+
     }
 
 }
