9584dac06ccd328941c70612b64e7ac28f13120a
Aseward1005 <anthony.seward1005@gmail.com>
2023-11-06 01:36:48 -0800

    added rough spell casting mechanics wind = uio fireball = uojil teleport = oulj

--- Assets/SpellController.cs
+++ Assets/SpellController.cs
@@ -1,7 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+
+public class Combo 
+{
+    public string[] buttons;
+    public int counter;
+    public int len;
+    public Combo(string[] buttonOrder)
+    { 
+        buttons = buttonOrder;
+        counter = 0;
+        len = buttonOrder.Length;
+    }
+
+    public bool checkButton() 
+    {
+        if (Input.GetButtonDown(buttons[counter]))
+        {
+            counter++;
+            if (counter == len)
+            {
+                counter = 0;
+                return true;
+            }
+        }
+
+        else
+        {
+            counter = 0;
+        }
+
+        return false;
+    }
+}
 
 public class SpellController : MonoBehaviour
 {
@@ -9,6 +43,10 @@ public class SpellController : MonoBehaviour
     public TeleportProperties Teleport;
     public FireLauncher Fire;
     public Vector3 dir;
+
+    private Combo windCombo = new(new string[] { "Spell1", "Spell2", "Spell3" });
+    private Combo fbCombo = new(new string[] { "Spell1", "Spell3", "Spell4", "Spell2", "Spell6" });
+    private Combo tpCombo = new(new string[] {"Spell3", "Spell1", "Spell6", "Spell4"});
 
     // Start is called before the first frame update
     void Start()
@@ -29,12 +67,33 @@ public class SpellController : MonoBehaviour
         setDirection(x, y);
 
         //string handling not added yet
+        /*
         if (Input.GetButtonDown("Spell1") == true)
             Wind.Push(dir);
         if (Input.GetButtonDown("Spell2") == true)
             Teleport.teleport(dir);
         if (Input.GetButtonDown("Spell3") == true)
             Fire.fire(dir);
+        */
+        if (spellButtonDown())
+        {
+            if (windCombo.checkButton())
+                Wind.Push(dir);
+            if (fbCombo.checkButton())
+                Fire.fire(dir);
+            if (tpCombo.checkButton())
+                Teleport.teleport(dir);
+        }
+
+    }
+
+    bool spellButtonDown()
+    {
+        if (Input.GetButtonDown("Spell1") || Input.GetButtonDown("Spell2") || Input.GetButtonDown("Spell3") || Input.GetButtonDown("Spell4") || Input.GetButtonDown("Spell5") || Input.GetButtonDown("Spell6"))
+            return true;
+        else 
+            return false;
+
     }
 
     void setDirection(float x, float y)
