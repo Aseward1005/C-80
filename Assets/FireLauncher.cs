3a1268ffd989d6797bde8a36e3f1782fbd1ae8a8
Aseward1005 <anthony.seward1005@gmail.com>
2023-11-05 18:51:31 -0800

    integration environment

--- Assets/FireLauncher.cs
+++ Assets/FireLauncher.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FireLauncher : MonoBehaviour
 {
     public float speed;
+    public float offset;
+
     public GameObject fireball;
     // Start is called before the first frame update
     void Start()
@@ -21,7 +23,7 @@ public class FireLauncher : MonoBehaviour
     //launch a fireball in a straight line
     public void fire(Vector3 dir)
     {
-        GameObject f = Instantiate(fireball, transform.position, transform.rotation);
+        GameObject f = Instantiate(fireball, transform.position + dir*offset, transform.rotation);
         f.GetComponent<Rigidbody2D>().velocity = dir * speed;
     }
 }
